4331d924f82c73827eff93653e56dd8ddea421ae
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-10-23 18:16:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,13 @@
 def reverse_each_word(sentence1)
   array=sentence1.split (" ")
+<<<<<<< HEAD
   array.collect do |word|
   word.reverse
   end.join (" ")
 end
+=======
+  array.each do |word|
+    word.reverse
+  end
+end
+>>>>>>> 707278f0388e81ec2706965d130325cf4a14dbce
